b940a22276b7d9a04da96ffc248b3a67d342039d
Balbino Soares Ferreira Filho <balbinosoares@cenibra.local>
2020-04-15 09:39:20 -0300

    App-Router and Docker configuration

--- approuter/index.js
+++ approuter/index.js
@@ -4,6 +4,7 @@ const config = require("./xs-app.json");
 config.routes.forEach((oRoute) => {
     "use strict";
 
+    // eslint-disable-next-line no-console
     if (oRoute.localDir && oRoute.localDir === "webapp") {
         oRoute.localDir = "../webapp";
     }
